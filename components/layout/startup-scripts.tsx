d6e93019fb9a6ffb06ccca94139dbb661a0bcc73
Nightshift Labs <nightshiftlabsza@gmail.com>
2026-03-13 22:49:14 +0200

    Force one-time service worker cache reset

--- components/layout/startup-scripts.tsx
+++ components/layout/startup-scripts.tsx
@@ -2,15 +2,24 @@ import Script from "next/script";
 
 const SAMPLE_PDF_CLEANUP_SCRIPT = `
 (() => {
+  const cacheHealKey = "lekkerledger-cache-heal-v2";
   const cleanupKey = "lekkerledger-sample-pdf-cleanup-v1";
   const pdfPath = "/sample-payslip.pdf";
 
   const runCleanup = async () => {
     const pdfUrl = new URL(pdfPath, window.location.origin).href;
     let removedCachedPdf = false;
+    let clearedAnyCache = false;
 
     if ("caches" in window) {
       const cacheNames = await caches.keys();
+
+      if (cacheNames.length > 0 && !sessionStorage.getItem(cacheHealKey)) {
+        await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName).then((deleted) => {
+          clearedAnyCache = deleted || clearedAnyCache;
+        })));
+      }
+
       for (const cacheName of cacheNames) {
         const cache = await caches.open(cacheName);
         const deletedAbsolute = await cache.delete(pdfUrl, { ignoreSearch: true });
@@ -23,10 +32,35 @@ const SAMPLE_PDF_CLEANUP_SCRIPT = `
       const registrations = await navigator.serviceWorker.getRegistrations();
       await Promise.all(registrations.map((registration) => registration.update().catch(() => undefined)));
 
+      if (registrations.length > 0 && !sessionStorage.getItem(cacheHealKey)) {
+        sessionStorage.setItem(cacheHealKey, "done");
+        await Promise.all(registrations.map((registration) => registration.unregister().catch(() => false)));
+        window.location.reload();
+        return;
+      }
+
       if (removedCachedPdf && !sessionStorage.getItem(cleanupKey)) {
         sessionStorage.setItem(cleanupKey, "done");
         await Promise.all(registrations.map((registration) => registration.unregister().catch(() => false)));
         window.location.reload();
+        return;
+      }
+    }
+
+    if (clearedAnyCache && !sessionStorage.getItem(cacheHealKey)) {
+      sessionStorage.setItem(cacheHealKey, "done");
+      window.location.reload();
+      return;
+    }
+
+    if (!sessionStorage.getItem(cacheHealKey)) {
+      sessionStorage.setItem(cacheHealKey, "done");
+      if ("serviceWorker" in navigator) {
+        const registrations = await navigator.serviceWorker.getRegistrations();
+        if (registrations.length > 0) {
+          await Promise.all(registrations.map((registration) => registration.unregister().catch(() => false)));
+          window.location.reload();
+        }
       }
     }
   };
